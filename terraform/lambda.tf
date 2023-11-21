1b86c90ca68640af38e084fc4726564254b553df
Michael French <michael.x.french@gmail.com>
2023-11-20 22:11:12 -0600

    use docker image in lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,19 +3,9 @@ module "nba_etl_lambda" {
 
   function_name = "update_player_gamelogs"
   description   = "Updates the NBA gamelogs for all players daily"
-  runtime       = "python3.12"
-  handler       = "update_gamelogs.lambda_handler"
-  source_path   = "../lambdas"
-}
 
-module "nba_etl_dependencies" {
-  source = "terraform-aws-modules/lambda/aws"
+  create_package = false
 
-  create_layer = true
-
-  layer_name          = "nba_etl_dependencies"
-  description         = "Python dependencies for the NBA ETL lambda"
-  compatible_runtimes = ["python3.12"]
-
-  source_path = "../packages"
+  image_uri    = "${aws_ecr_repository.nba_etl.repository_url}@${data.aws_ecr_image.nba_etl_image.id}"
+  package_type = "Image"
 }
