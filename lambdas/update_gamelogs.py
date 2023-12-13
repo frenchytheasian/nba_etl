3ee82e7527f492bd789b02770ff37ea3d09d646e
Michael French <michael.x.french@gmail.com>
2023-12-12 22:00:20 -0600

    get yesterdays games

--- lambdas/update_gamelogs.py
+++ lambdas/update_gamelogs.py
@@ -1,16 +1,26 @@
-import json
+from datetime import datetime
 
 import pandas as pd
+from nba_api.stats.endpoints import scoreboardv2, boxscoretraditionalv3
+from nba_api.stats.library.parameters import DayOffset
+
+def _get_box_score_for_game(game_id: str) -> pd.DataFrame:
+    box_score_results = boxscoretraditionalv3.BoxScoreTraditionalV3(game_id=game_id)
+    return box_score_results.get_data_frames()[0]
 
 
 def lambda_handler(event, context):
-    fake_data_for_df = {
-        'pla': ['a', 'b', 'c'],
-    }
-    df = pd.DataFrame(fake_data_for_df)
-    print(df.head())
+    scoreboard_results = scoreboardv2.ScoreboardV2(day_offset=-1)
+    game_header_df = scoreboard_results.game_header.get_data_frame()
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    games_done = (game_header_df["GAME_STATUS_TEXT"] == "Final").all()
+    if not games_done:
+        raise ValueError("Not all games are done yet")
+
+    game_ids = game_header_df["GAME_ID"].to_list()
+
+    box_scores = []
+    for game_id in game_ids:
+        box_scores.append(_get_box_score_for_game(game_id))
+    
+    return pd.concat(box_scores, ignore_index=True)
